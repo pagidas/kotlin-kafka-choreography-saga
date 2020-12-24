f4b0008514adeccd4361f7f759993a3d9064990e
pagidas <pagakrivos@gmail.com>
2020-12-24 17:20:15 +0200

    Puts dummy pantry items data in postgres in db migration

---
+++ db-migrations/src/main/resources/db/migration/V3__inserts_demo_pantry_items.sql
@@ -0,0 +1,6 @@
+INSERT INTO pantry_app.items
+    VALUES (gen_random_uuid(), 'broccoli', 1);
+INSERT INTO pantry_app.items
+    VALUES (gen_random_uuid(), 'tomatoes', 5);
+INSERT INTO pantry_app.items
+    VALUES (gen_random_uuid(), 'cucumber', 2);
